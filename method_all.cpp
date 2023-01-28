fa8b26442ff407440f9161eaa5a80a7d9923dcf7
amatur <amatur003@gmail.com>
2023-01-28 16:26:43 -0500

    fixed_mini_dlc

--- method_all.cpp
+++ method_all.cpp
@@ -900,14 +900,14 @@ public:
 					if(skip!=0){ 	//not skipped, run break, write lm
 						//paul method
 						{
-							int q, rem;
-							q = floor(skip/max_run);
-							rem = skip % max_run;
-							assert(skip == q*max_run + rem); //skip = q*max_run + rem
-							write_number_at_loc(positions, CATEGORY_RUN, (uint64_t) 2, b_it);
-							write_unary_one_at_loc(positions, (uint64_t) q, b_it);
-							write_zero(positions, b_it);
-							write_number_at_loc(positions, (uint64_t) rem, (uint64_t) lmaxrun, b_it);
+							// int q, rem;
+							// q = floor(skip/max_run);
+							// rem = skip % max_run;
+							// assert(skip == q*max_run + rem); //skip = q*max_run + rem
+							// write_number_at_loc(positions, CATEGORY_RUN, (uint64_t) 2, b_it);
+							// write_unary_one_at_loc(positions, (uint64_t) q, b_it);
+							// write_zero(positions, b_it);
+							// write_number_at_loc(positions, (uint64_t) rem, (uint64_t) lmaxrun, b_it);
 						}
 						
 						//my method
