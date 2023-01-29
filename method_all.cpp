769242630066782f562ccc4b0866811acbe1274b
amatur <amatur003@gmail.com>
2023-01-29 18:32:52 -0500

    fixed_mini_dlc_now_codecomplete

--- method_all.cpp
+++ method_all.cpp
@@ -139,9 +139,9 @@ class Hashtable {
 
 	vector<uint32_t> get_array(){
 		vector<uint32_t> array(curr_id, 0);
-		for( const auto& [key, value] : htmap )
-       		array[value] =  key ;
-			cout<<key<<"->"<<value<<endl;
+		for (auto x : htmap){
+			array[x.second] =  x.first  ;
+			cout<<x.first<<"->"<<x.second<<endl;
 		}
 		return array;
 	}
